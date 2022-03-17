f48ccf6d67ae2a209a1cfaa66a2c3f46e7eb1f43
Damian Plewa <damianp96@gmail.com>
2022-03-17 15:23:15 +0100

    refactor(main): added log levels

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { LogLevel } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createApplicationContext(AppModule);
+  const logLevels: LogLevel[] = ['error', 'warn', 'log', 'verbose', 'debug'];
+  const LOG_LEVEL = +(process.env.LOG_LEVEL ?? '4');
+  logLevels.splice(LOG_LEVEL);
+
+  const app = await NestFactory.createApplicationContext(AppModule, {
+    logger: logLevels,
+  });
   await app.init();
 }
 bootstrap();
