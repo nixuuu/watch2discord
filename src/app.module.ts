7c27444e5abbb3b73e6bdc32905bb8e3e8576aaf
Damian Plewa <damianp96@gmail.com>
2022-03-17 12:24:28 +0100

    fix(bot): removed nest hello world code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { BotModule } from './modules/bot/bot.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), BotModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
